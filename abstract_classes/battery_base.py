fa3aaa9703318f1078f74cd63d3c58a373b75290
David <wd3056@mci4me.at>
2026-07-07 19:31:29 +0200

    Erstellen der Klasse battery_pack

--- abstract_classes/battery_base.py
+++ abstract_classes/battery_base.py
@@ -6,9 +6,6 @@ class BatteryBase(ABC):
     def __init__(self, capacity_nom_Ah: float, initial_soc: float = 1.0):
         self.C_nom = capacity_nom_Ah * 3600.0  # Kapazität in As
         self.soc = initial_soc
-        self.R_int = 0.08
-        self.Vmin = 32.0
-        self.Vmax = 42.0
 
     @abstractmethod
     def apply_current(self, current: float, duration: float) -> None:
